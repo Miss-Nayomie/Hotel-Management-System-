5f52337d032d774524736529f0741e3ca8222278
Miss-Nayomie <naominyongesa2017@gmail.com>
2023-01-25 22:42:29 +0300

    Day 25 commit

--- src/main/java/com/nayomie/cms_backend/controllerImpl/UserControllerImpl.java
+++ src/main/java/com/nayomie/cms_backend/controllerImpl/UserControllerImpl.java
@@ -4,11 +4,14 @@ import com.nayomie.cms_backend.constants.CafeConstant;
 import com.nayomie.cms_backend.controller.UserController;
 import com.nayomie.cms_backend.service.UserService;
 import com.nayomie.cms_backend.utils.CafeUtils;
+import com.nayomie.cms_backend.wrapper.UserWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -23,5 +26,15 @@ public class UserControllerImpl implements UserController {
             ex.printStackTrace();
         }
         return CafeUtils.getResponseEntity(CafeConstant.SOMETHING_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    public ResponseEntity<List(Map<UserWrapper> getAllUSer()) {
+        try {
+            return userService.getAllUser;
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return new ResponseEntity<List<UserWrapper>>(new ArrayList<>(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
